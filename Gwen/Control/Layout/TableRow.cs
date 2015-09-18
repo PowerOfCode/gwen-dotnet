b5130b9bc08216e68d7ef96d976adb4119f0632a
benklett <b.klettbach@gmail.com>
2015-09-18 14:32:26 +0200

    Add new Layout-Table

--- Gwen/Control/Layout/TableRow.cs
+++ Gwen/Control/Layout/TableRow.cs
@@ -1,221 +1,76 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 
 namespace Gwen.Control.Layout
 {
-    /// <summary>
-    /// Single table row.
-    /// </summary>
-    public class TableRow : ControlBase
-    {
-        // [omeg] todo: get rid of this
-        public const int MaxColumns = 5;
+	/// <summary>
+	/// Single table row.
+	/// </summary>
+	public class TableRow : ControlBase
+	{
+		public TableRow(ControlBase parent)
+			: base(parent)
+		{
+		}
 
-        private int m_ColumnCount;
-        private bool m_EvenRow;
-        private readonly Label[] m_Columns;
+		protected override void layout(Gwen.Skin.SkinBase skin)
+		{
+			Table parent = Parent as Table;
 
-        internal Label GetColumn(int index)
-        {
-            return m_Columns[index];
-        }
+            Height = 0;
+            Width = 0;
 
-        /// <summary>
-        /// Invoked when the row has been selected.
-        /// </summary>
-        public event GwenEventHandler<ItemSelectedEventArgs> Selected;
-
-        /// <summary>
-        /// Column count.
-        /// </summary>
-        public int ColumnCount { get { return m_ColumnCount; } set { SetColumnCount(value); } }
-
-        /// <summary>
-        /// Indicates whether the row is even or odd (used for alternate coloring).
-        /// </summary>
-        public bool EvenRow { get { return m_EvenRow; } set { m_EvenRow = value; } }
-
-        /// <summary>
-        /// Text of the first column.
-        /// </summary>
-        public string Text { get { return GetText(0); } set { SetCellText(0, value); } }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TableRow"/> class.
-        /// </summary>
-        /// <param name="parent">Parent control.</param>
-        public TableRow(ControlBase parent)
-            : base(parent)
-        {
-            m_Columns = new Label[MaxColumns];
-            m_ColumnCount = 0;
-            KeyboardInputEnabled = true;
-        }
-
-        /// <summary>
-        /// Sets the number of columns.
-        /// </summary>
-        /// <param name="columnCount">Number of columns.</param>
-        protected void SetColumnCount(int columnCount)
-        {
-            if (columnCount == m_ColumnCount) return;
-
-            if (columnCount >= MaxColumns)
-                throw new ArgumentException("Invalid column count", "columnCount");
-
-            for (int i = 0; i < MaxColumns; i++)
-            {
-                if (i < columnCount)
+            // Fill ColumnWidths List
+			if (parent.Children[0] == this)
+			{
+				parent.ColumnWidths.Clear();
+                for (int columnIndex = 0; columnIndex < Children.Count; columnIndex++)
                 {
-                    if (null == m_Columns[i])
+                    int colWidth = 0;
+
+                    for (int rowIndex = 0; rowIndex < parent.Children.Count; rowIndex++)
                     {
-                        m_Columns[i] = new Label(this);
-                        m_Columns[i].Padding = Padding.Three;
-                        m_Columns[i].Margin = new Margin(0, 0, 2, 0); // to separate them slightly
-                        if (i == columnCount - 1)
-                        {
-                            // last column fills remaining space
-                            m_Columns[i].Dock = Pos.Fill;
-                        }
-                        else
-                        {
-                            m_Columns[i].Dock = Pos.Left;
-                        }
+                        ControlBase element = parent.Children[rowIndex].Children[columnIndex];
+
+                        colWidth = Math.Max(element.Width + element.Margin.Width(), colWidth);
                     }
+
+                    parent.ColumnWidths.Add(colWidth);
                 }
-                else if (null != m_Columns[i])
-                {
-                    RemoveChild(m_Columns[i], true);
-                    m_Columns[i] = null;
-                }
+			}
 
-                m_ColumnCount = columnCount;
-            }
-        }
-
-        /// <summary>
-        /// Sets the column width (in pixels).
-        /// </summary>
-        /// <param name="column">Column index.</param>
-        /// <param name="width">Column width.</param>
-        public void SetColumnWidth(int column, int width)
-        {
-            if (null == m_Columns[column]) 
-                return;
-            if (m_Columns[column].Width == width) 
-                return;
-
-            m_Columns[column].Width = width;
-        }
-
-        /// <summary>
-        /// Sets the text of a specified cell.
-        /// </summary>
-        /// <param name="column">Column number.</param>
-        /// <param name="text">Text to set.</param>
-        public void SetCellText(int column, string text)
-        {
-            if (null == m_Columns[column]) 
-                return;
-
-            m_Columns[column].Text = text;
-        }
-
-        /// <summary>
-        /// Sets the contents of a specified cell.
-        /// </summary>
-        /// <param name="column">Column number.</param>
-        /// <param name="control">Cell contents.</param>
-        /// <param name="enableMouseInput">Determines whether mouse input should be enabled for the cell.</param>
-        public void SetCellContents(int column, ControlBase control, bool enableMouseInput = false)
-        {
-            if (null == m_Columns[column]) 
-                return;
-
-            control.Parent = m_Columns[column];
-            m_Columns[column].MouseInputEnabled = enableMouseInput;
-        }
-
-        /// <summary>
-        /// Gets the contents of a specified cell.
-        /// </summary>
-        /// <param name="column">Column number.</param>
-        /// <returns>Control embedded in the cell.</returns>
-        public ControlBase GetCellContents(int column)
-        {
-            return m_Columns[column];
-        }
-
-        protected virtual void OnRowSelected()
-        {
-            if (Selected != null)
-                Selected.Invoke(this, new ItemSelectedEventArgs(this));
-        }
-
-        /// <summary>
-        /// Sizes all cells to fit contents.
-        /// </summary>
-        public void SizeToContents()
-        {
-            int width = 0;
-            int height = 0;
-
-            for (int i = 0; i < m_ColumnCount; i++)
+            // Set Width and Height of this row
+            foreach (var child in Children)
             {
-                if (null == m_Columns[i]) 
-                    continue;
+                int height = child.Height + child.Margin.Height();
 
-                // Note, more than 1 child here, because the 
-                // label has a child built in ( The Text )
-                if (m_Columns[i].Children.Count > 1)
-                {
-                    m_Columns[i].SizeToChildren();
-                }
-                else
-                {
-                    m_Columns[i].SizeToContents();
-                }
-    
-                //if (i == m_ColumnCount - 1) // last column
-                //    m_Columns[i].Width = Parent.Width - width; // fill if not autosized
-
-                width += m_Columns[i].Width + m_Columns[i].Margin.Left + m_Columns[i].Margin.Right;
-                height = Math.Max(height, m_Columns[i].Height + m_Columns[i].Margin.Top + m_Columns[i].Margin.Bottom);
+                if (height > Height)
+                    Height = height;
             }
 
-            SetSize(width, height);
-        }
+            Width = parent.ColumnWidths.Sum();
 
-        /// <summary>
-        /// Sets the text color for all cells.
-        /// </summary>
-        /// <param name="color">Text color.</param>
-        public void SetTextColor(Color color)
-        {
-            for (int i = 0; i < m_ColumnCount; i++)
+            // Set cell positions
+			int shift = 0;
+            if (Children.Count != 0)
             {
-                if (null == m_Columns[i]) continue;
-                m_Columns[i].TextColor = color;
+                ControlBase child = Children[0];
+
+                shift = child.Margin.Left;
+                child.SetPosition(shift, child.Margin.Top);
+
+                for (int i = 1; i < Children.Count; i++)
+                {
+                    child = Children[i];
+
+                    shift += parent.ColumnWidths[i - 1];
+
+                    child.SetPosition(shift, child.Margin.Top);
+                }
             }
-        }
 
-        /// <summary>
-        /// Returns text of a specified row cell (default first).
-        /// </summary>
-        /// <param name="column">Column index.</param>
-        /// <returns>Column cell text.</returns>
-        public string GetText(int column = 0)
-        {
-            return m_Columns[column].Text;
-        }
-
-        /// <summary>
-        /// Handler for Copy event.
-        /// </summary>
-        /// <param name="from">Source control.</param>
-        protected override void onCopy(ControlBase from, EventArgs args)
-        {
-            Platform.Neutral.SetClipboardText(Text);
-        }
-    }
+			base.layout(skin);
+		}
+	}
 }
